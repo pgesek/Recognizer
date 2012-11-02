7e104759bb4eb00cf55c1d4624595d32596292d8
Pawel Gesek <pgesek@gmail.com>
2012-11-03 00:57:46 +0100

    Commit in missing files

---
+++ Recognizer/IO/FileReader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Recognizer.IO
+{
+    class FileReader : IInputReader
+    {
+        private string filePath;
+        private Encoding encoding;
+
+        public FileReader(string filePath) : this(filePath, Encoding.Default) { }
+
+        public FileReader(string filePath, Encoding encoding)
+        {
+            this.filePath = filePath;
+            this.encoding = encoding;
+        }
+
+        #region ISentenceReader Members
+
+        public string ReadInput()
+        {
+            return File.ReadAllText(filePath, encoding);
+        }
+
+        #endregion
+    }
+}
---
+++ Recognizer/IO/StdinReader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Recognizer.IO
+{
+    class StdinReader : IInputReader
+    {
+        #region ISentenceReader Members
+
+        public string ReadInput()
+        {
+            StringBuilder sb = new StringBuilder();
+            string line;
+
+            while ((line = Console.ReadLine()) != "\\q") 
+            {
+                sb.AppendLine(line);    
+            }
+
+            return sb.ToString();
+        }
+
+        #endregion
+    }
+}
